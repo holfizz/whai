08a5c0fc61e0492029109050319e8be0e9dbd3ee
holfizz <gorlach7v@gmial.com>
2024-08-24 21:14:54 +0300

    feat: added number protection

--- src/features/auth/model/auth.contracts.ts
+++ src/features/auth/model/auth.contracts.ts
@@ -1,5 +1,6 @@
 'use client'
 import { z } from 'zod'
+const phoneNumberRegex = /^\+7 \(\d{3}\) \d{3} \d{2}-\d{2}$/
 
 export const formSignUpSchema = z.object({
 	email: z.string().email('Email is not correct'),
@@ -14,9 +15,13 @@ export const formSignUpSchema = z.object({
 		.min(2, { message: 'Last name must be at least 2 characters' }),
 	phoneNumber: z
 		.string()
-		.min(9, { message: 'Phone number must be at least 10 characters' })
+		.min(18, { message: 'Phone number must be exactly 18 characters long' })
+		.max(18, { message: 'Phone number must be exactly 18 characters long' })
+		.regex(
+			phoneNumberRegex,
+			'Phone number must be in the format +7 (XXX) XXX XX-XX'
+		)
 })
-
 export const formLoginSchema = z.object({
 	email: z.string().email('Email is not correct'),
 	password: z
