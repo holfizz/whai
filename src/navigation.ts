c84629cdc6259e708cbcd24f63b87ec021425771
holfizz <gorlach7v@gmial.com>
2024-09-19 21:41:43 +0300

    fix

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,6 +1,6 @@
 import { createSharedPathnamesNavigation } from 'next-intl/navigation'
 
-export const locales = ['ru', 'end'] as const
+export const locales = ['ru', 'en'] as const
 export const localePrefix = 'as-needed'
 
 export const { Link, redirect, usePathname, useRouter } =
